2252cfb22fc6e18c4bb83b4aaca0e734907b42b9
kritag <3776749+kritag@users.noreply.github.com>
2026-01-16 12:50:56 +0100

    asd

--- sudo.py
+++ sudo.py
@@ -1,63 +1,67 @@
-import subprocess, dotbot, json
-from os import path, remove, close, unlink
-from dotbot.util import module
+import json
+import subprocess
+import sys
 from io import open
+from os import close, path, remove
 from tempfile import mkstemp
+
+from dotbot.util import module
+
+import dotbot
 
 
 class Sudo(dotbot.Plugin):
-    _directive = 'sudo'
+    _directive = "sudo"
 
     def can_handle(self, directive):
         return self._directive == directive
 
     def handle(self, directive, data):
         if directive != self._directive:
-            raise ValueError('sudo cannot handle directive %s' %
-                directive)
+            raise ValueError("sudo cannot handle directive %s" % directive)
 
         app = self._find_dotbot()
         base_directory = self._context.base_directory()
-        data = [{'defaults': self._context.defaults()}] + data
+        data = [{"defaults": self._context.defaults()}] + data
         plugins = self._collect_plugins()
 
         try:
-            fd, sudo_conf = mkstemp(prefix='dotbot-sudo.', suffix='.json')
-            self._log.debug('sudo: temporary config path: {}'.format(sudo_conf))
+            fd, sudo_conf = mkstemp(prefix="dotbot-sudo.", suffix=".json")
+            self._log.debug("sudo: temporary config path: {}".format(sudo_conf))
             self._write_conf_file(sudo_conf, data)
 
             proc_args = [
-                'sudo', app,
-                '--base-directory', base_directory,
-                '--config-file', sudo_conf
-                ] + plugins
-            self._log.debug('sudo: args to pass: {}'.format(proc_args))
+                "sudo",
+                app,
+                "--base-directory",
+                base_directory,
+                "--config-file",
+                sudo_conf,
+            ] + plugins
+            self._log.debug("sudo: args to pass: {}".format(proc_args))
 
             try:
-                self._log.lowinfo('sudo: begin subprocess')
-                subprocess.check_call(
-                    proc_args,
-                    stdin=subprocess.PIPE)
-                self._log.lowinfo('sudo: end subprocess')
+                self._log.lowinfo("sudo: begin subprocess")
+                subprocess.check_call(proc_args, stdin=subprocess.PIPE)
+                self._log.lowinfo("sudo: end subprocess")
                 self._delete_conf_file(sudo_conf)
                 return True
             except subprocess.CalledProcessError as e:
-                self._log.lowinfo('sudo: end subprocess')
+                self._log.lowinfo("sudo: end subprocess")
                 self._log.error(e)
                 return False
         finally:
-            self._log.debug('sudo: garbage collecting: {}'.format(sudo_conf))
+            self._log.debug("sudo: closing fd: {}".format(sudo_conf))
             close(fd)
-            unlink(fd)
 
     def _collect_plugins(self):
         ret = []
         for plugin in module.loaded_modules:
             # HACK should we compare to something other than _directive?
             if plugin.__name__ != self._directive:
-                ret.extend(iter([
-                    '--plugin',
-                    path.splitext(plugin.__file__)[0] + '.py']))
+                ret.extend(
+                    iter(["--plugin", path.splitext(plugin.__file__)[0] + ".py"])
+                )
         return ret
 
     def _delete_conf_file(self, conf_file):
@@ -65,14 +69,12 @@ class Sudo(dotbot.Plugin):
             remove(conf_file)
 
     def _find_dotbot(self):
-        base = path.dirname(path.dirname(dotbot.__file__))
-        ret = path.join(base, '..', 'bin', 'dotbot')
-        self._log.debug('sudo: dotbot app path: {}'.format(ret))
-        return ret
+        self._log.debug("sudo: dotbot app path: {}".format(sys.argv[0]))
+        return sys.argv[0]
 
     def _write_conf_file(self, conf_file, data):
         self._delete_conf_file(conf_file)
-        with open(conf_file, 'w', encoding='utf-8') as jfile:
+        with open(conf_file, "w", encoding="utf-8") as jfile:
             my_json_str = json.dumps(data, ensure_ascii=False)
             if isinstance(my_json_str, str):
                 my_json_str = my_json_str.encode().decode("utf-8")
@@ -81,15 +83,16 @@ class Sudo(dotbot.Plugin):
 
     @staticmethod
     def strip_args(args):
-        single_args_filter = [
-            '--version',
-            '-h', '--help'
-        ]
+        single_args_filter = ["--version", "-h", "--help"]
 
         double_args_filter = [
-            '-d', '--base-directory',
-            '-c', '--config-file',
-            '-p', '--plugin', '--plugin-dir'
+            "-d",
+            "--base-directory",
+            "-c",
+            "--config-file",
+            "-p",
+            "--plugin",
+            "--plugin-dir",
         ]
 
         args_filter = single_args_filter + double_args_filter
